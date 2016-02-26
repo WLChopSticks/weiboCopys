a9a0cb4f53f35a04dc02bcb9e7cbd4256508fea8
WLChopSticks <wanglei_sy56@163.com>
2016-02-26 15:21:41 +0800

    转发评论栏增加图片以及分割线

--- weibo/weibo/Classes/Home/View/cell/WLCOriginalStatusBottomView.m
+++ weibo/weibo/Classes/Home/View/cell/WLCOriginalStatusBottomView.m
@@ -9,7 +9,7 @@
 #import "WLCOriginalStatusBottomView.h"
 #import "Masonry.h"
 
-#define BORDER_COLOR RGB(216, 216, 216).CGColor
+#define BORDER_COLOR RGB(216, 216, 216)
 
 @implementation WLCOriginalStatusBottomView
 
@@ -18,7 +18,7 @@
         
         [self decorateUI];
         self.layer.borderWidth = 0.5;
-        self.layer.borderColor = BORDER_COLOR;
+        self.layer.borderColor = BORDER_COLOR.CGColor;
     }
     
     return self;
@@ -30,18 +30,34 @@
     UIButton *reposetBtn = [[UIButton alloc]init];
     [reposetBtn setTitle:@"转发" forState:UIControlStateNormal];
     [reposetBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [reposetBtn setImage:[UIImage imageNamed:@"timeline_icon_retweet"] forState:UIControlStateNormal];
+    [reposetBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background"] forState:UIControlStateNormal];
+    [reposetBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background_highlighted"] forState:UIControlStateHighlighted];
     [self addSubview:reposetBtn];
     
     UIButton *commentsBtn = [[UIButton alloc]init];
     [commentsBtn setTitle:@"评论" forState:UIControlStateNormal];
     [commentsBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [commentsBtn setImage:[UIImage imageNamed:@"timeline_icon_comment"] forState:UIControlStateNormal];
+    [commentsBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background"] forState:UIControlStateNormal];
+    [commentsBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background_highlighted"] forState:UIControlStateHighlighted];
     [self addSubview:commentsBtn];
     
     UIButton *attitudesBtn = [[UIButton alloc]init];
     [attitudesBtn setTitle:@"赞" forState:UIControlStateNormal];
     [attitudesBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [attitudesBtn setImage:[UIImage imageNamed:@"timeline_icon_unlike"] forState:UIControlStateNormal];
+    [attitudesBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background"] forState:UIControlStateNormal];
+    [attitudesBtn setBackgroundImage:[UIImage imageNamed:@"timeline_retweet_background_highlighted"] forState:UIControlStateHighlighted];
     [self addSubview:attitudesBtn];
     
+    UIView *seperateViewL = [[UIView alloc]init];
+    seperateViewL.backgroundColor = BORDER_COLOR;
+    [self addSubview:seperateViewL];
+    
+    UIView *seperateViewR = [[UIView alloc]init];
+    seperateViewR.backgroundColor = BORDER_COLOR;
+    [self addSubview:seperateViewR];
     
     //约束
     CGFloat btnWidth = ScreenWidth / 3.0;
@@ -67,6 +83,20 @@
         make.left.equalTo(commentsBtn.mas_right);
         make.bottom.equalTo(self.mas_bottom);
         make.width.mas_equalTo(btnWidth);
+    }];
+    
+    //分割线约束
+    [seperateViewL mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top).offset(6);
+        make.left.equalTo(reposetBtn.mas_right);
+        make.bottom.equalTo(self.mas_bottom).offset(-6);
+        make.width.mas_equalTo(0.5);
+    }];
+    [seperateViewR mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.mas_top).offset(6);
+        make.left.equalTo(commentsBtn.mas_right);
+        make.bottom.equalTo(self.mas_bottom).offset(-6);
+        make.width.mas_equalTo(0.5);
     }];
     
 }
